7f68a3446fa51610a1a56aa6f95b3a9f9e59e97c
Rafael Pimenta <jrafapimenta@gmail.com>
2022-10-09 13:44:41 +0100

    start api

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flask_restful import Api
+from flask_restful import Resource, reqparse, abort, marshal_with, fields
 
 app = Flask(__name__)
+
+api = Api(app)
+
+# Backen Api
+communication_args = reqparse.RequestParser()
+
 
 # Routes
 @app.route('/')
